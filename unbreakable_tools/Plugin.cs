d61d30af3d2701de9e0708fd3c9b6b08f4fafdb0
devopsdinosaur <devopsdinosaur@gmail.com>
2022-10-13 01:21:27 -0400

    Works but ignores config settings

--- unbreakable_tools/Plugin.cs
+++ unbreakable_tools/Plugin.cs
@@ -10,36 +10,39 @@ using UnityEngine;
 using System.IO;
 
 
-[BepInPlugin("unbreakable_tools", "Unbreakable Tools", "0.0.1")]
+[BepInPlugin("devopsdinosaur.dinkum.unbreakable_tools", "Unbreakable Tools", "0.0.1")]
 public class Plugin : BaseUnityPlugin {
 
-	public static string NAME = "unbreakable_tools";
-	public static string TITLE = "Unbreakable Tools";
-	public static string VERSION = "0.0.1";
-	public static string AUTHOR = "0.0.1";
-
-	private Harmony m_harmony = new Harmony(NAME);
+	private Harmony m_harmony = new Harmony("devopsdinosaur.dinkum.unbreakable_tools");
 	public static ManualLogSource logger;
 
 	private ConfigEntry<bool> m_is_enabled;
+	private ConfigEntry<float> m_durability_loss_multiplier;
 
 
 	public Plugin() {
 	}
 
-	private void info_log(string text) {
-		Plugin.logger.LogInfo((object) text);
-	}
-
 	private void Awake() {
 		Plugin.logger = this.Logger;
-		this.info_log(string.Format("'%s' v%s plugin loaded.", TITLE, VERSION));
+		logger.LogInfo((object) "devopsdinosaur.dinkum.unbreakable_tools v0.0.1 loaded.");
 		this.m_harmony.PatchAll();
 		this.m_is_enabled = this.Config.Bind<bool>("General", "Enabled", true, "Set this to false to disable this mod.");
-
+		this.m_durability_loss_multiplier = this.Config.Bind<float>("Options", "Durability Loss Multiplier", 0.0f, "Multiplied times 'fuelOnUse' field to calculate amount of durability loss for each use of the tool.\n- A value of 1 indicates normal behavior\n- Less than one reduces durability loss (ex: 0.5 reduces loss to 1/2)\n- 0 (default) removes all durability loss.");
 	}
 
 	private void Start() {
-		
+		if (!this.m_is_enabled.Value) {
+			return;
+		}
+		foreach (InventoryItem item in Inventory.inv.allItems) {
+			if (item.isATool) {
+				logger.LogInfo((object) item.itemName);
+				//logger.LogInfo((object) this.m_durability_loss_multiplier.Value.ToString());
+				//item.fuelOnUse = (int) Math.Floor((float) item.fuelOnUse * this.m_durability_loss_multiplier.Value);
+				item.fuelOnUse = 0;
+				//logger.LogInfo((object) item.fuelOnUse.ToString());
+			}
+		}
 	}
 }
